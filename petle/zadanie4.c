06d73dbc7caa2b8ed41480a84a93da9f1f56fcac
Fr3sh98 <44109742+Fr3sh98@users.noreply.github.com>
2019-01-12 08:31:06 +0100

    Update zadanie4.c

--- petle/zadanie4.c
+++ petle/zadanie4.c
@@ -1,27 +1,30 @@
 
 //4. Napisz program, który wyliczy sumę liczb naturalnych od 1 do liczby n podanej przez użytkownika.
 //Kiedy użytkownik poda liczbę mniejszą niż 1, to program powinien wypisać stosowny komunikat o błędzie.
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
+void wczytaj_liczbe_calkowita(int *n_p){
+  printf("Podaj liczbę całkowitą większą od 0: ");
+  scanf("%d", n_p);
 
-  int n, a, suma = 0;
-  printf("Podaj liczbe: ");
-  scanf("%d", &n);
+if (*n_p <= 0) {
+  printf("Liczba musi być większa od 0.\n");
+  exit(1);
+  }
+}
 
-  if (n < 1)
-  {
-    printf("Podaj liczbe wieksza od 0!\n");
-    exit(1);
+int main() {
+int n, suma = 0;
+int i; //kolejne liczby od 1
+
+wczytaj_liczbe_calkowita(&n);
+
+i = 1;
+while (i <= n) {
+  suma += i;
+  i++;
   }
-  else
-    ;
-  for (a = 1; a <= n; ++a)
-  {
-    suma += a;
-  }
-  printf("Suma liczb = %d\n", suma);
+
+  printf("Suma: %d\n", suma);
 }
