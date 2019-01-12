feef61c6fc4314bf1e42da56922a021c0b1eebd3
Fr3sh98 <44109742+Fr3sh98@users.noreply.github.com>
2019-01-12 08:37:55 +0100

    Update zadanie5.c

--- petle/zadanie5.c
+++ petle/zadanie5.c
@@ -3,28 +3,13 @@
 // a następnie wypisze zawartość tej tablicy od początku i od końca.
 
 #include <stdio.h>
-#include <stdlib.h>
 
-#define size 6
+int main() {
+  int n = 8;
+  int t[n];
 
-int main()
-{
-  int tab[size];
-  int i;
+  printf("Podaj %d liczb całkowitych:\n", n);
+  scanf("%d, %d, %d, %d, %d, %d", &t[0], &t[1], &t[2], &t[3], &t[4], &t[5]);
 
-  printf("Podaj 6 elementow tablicy!\n");
-  for (i = 0; i < size; i++)
-  {
-    scanf("%d", &tab[i]);
-  }
-  printf("Elementy tablicy od poczatku: \n");
-  for (i = 0; i < size; i++)
-  {
-    printf("%d\n", tab[i]);
-  }
-  printf("Elementy tablicy od konca: \n");
-  for (i = size - 1; i >= 0; i--)
-  {
-    printf("%d\n", tab[i]);
-  }
+  printf("Od końca: %d, %d, %d, %d, %d, %d\n", t[5], t[4], t[3], t[2], t[1], t[0]);
 }
