a279acb9b7921b7edc3cb364f933785b6bf7a690
Fr3sh98 <44109742+Fr3sh98@users.noreply.github.com>
2019-01-26 10:03:04 +0100

    Update zadanie3a.c

--- Advent-of-Code/zadanie3a.c
+++ Advent-of-Code/zadanie3a.c
@@ -2,45 +2,83 @@
 
 //deklaracja tablicy 1000x1000 potrzebej do zadnia nr. 3
 int fabric[1000][1000];
+//tablica do zliczania znalezionych liczb
+int numberFound[200000];
+const int letters = 'z' - 'a' + 1; //ilość liter w alfabecie angielskim
 
-void f3() {
-//deklaracja oraz otwarcie pliku z danymi
-    FILE *file = fopen("data3", "r");
-//deklaracja zmiennych które pozwolą nam się 'poruszać' po dwuwymiarowej tablicy
-    int l = 0, t = 0, w = 0, h = 0;
-    char currentline[50];
-
-//wypełnianie naszej tablicy zerami
-    for (int i = 0; i < 1000; i++)
-        for (int j = 0; j < 1000; j++)
-            fabric[i][j] = 0;
-
-//pętla przechodząca przez plik i wczytująca wszystkie linie aż do końca pliku
-    while (fgets(currentline, sizeof(currentline), file) != NULL) {
-//'wyciąganie' danych z linii
-        sscanf(currentline, "%d%d%d%d", &l, &t, &w, &h);
-//zliczanie ilości punktów które należą do danego elfa
-        for (int y = t; y < t + h; y++)
-            for (int x = l; x < w + l; x++)
-                fabric[y][x]++;
-    }
-
-    int area = 0;
-
-//sprawdzamy wszystkie pola w tablicy, jezeli sa wieksze od 1 do zmiennej area dodajemy 1
-    for (int i = 0; i < 1000; i++)
-        for (int j = 0; j < 1000; j++)
-            if (fabric[i][j] > 1)
-                area++;
-
-    printf("3) %d\n", area);
-
-//zamknięcie pliku
-    fclose(file);
-  }
-
+void f3();
 
 int main() {
     f3();
     return 0;
+}
+
+
+
+void reset() {
+    //wypelnianie naszej tablicy zerami
+    for (int i = 0; i < 1000; i++)
+        for (int j = 0; j < 1000; j++)
+            fabric[i][j] = 0;
+}
+
+void prepareData() {
+    FILE *file = fopen("data3", "r"); //deklaracja oraz otwarcie pliku z danymi
+    int l = 0, t = 0, w = 0, h = 0;//deklaracja zmiennych ktore pozwola nam sie 'poruszac' po dwuwymiarowej tablicy
+    char currentline[50];
+
+    //petla przechodzaca przez plik i wczytujaca wszystkie linie az do konca pliku
+    while (fgets(currentline, sizeof(currentline), file) != NULL) {
+        sscanf(currentline, "%d%d%d%d", &l, &t, &w, &h);//'wyciaganie' danych z linii
+        for (int y = t; y < t + h; y++)
+            for (int x = l; x < w + l; x++)
+                fabric[y][x]++; //zliczanie ilosci punktow ktore naleza do danego elfa
+    }
+    fclose(file);//zamkniecie pliku
+}
+
+void f3a() {
+    int area = 0;
+
+    reset();
+    prepareData();
+
+    for (int i = 0;
+         i < 1000; i++)//sprawdzamy wszystkie pola w tablicy, jezeli sa wieksze od 1, to do zmiennej area dodajemy 1
+        for (int j = 0; j < 1000; j++)
+            if (fabric[i][j] > 1)
+                area++;
+    printf("3) a. %d", area);
+}
+
+int sharedBy(int l, int t, int w, int h) {
+    int shared = 0;//zliczanie ilosci punktow ktore sa wspoldzielone przez wiecej niz jednego elfa
+
+    for (int y = t; y < t + h; y++)
+        for (int x = l; x < w + l; x++)
+            if (fabric[y][x] > 1)//sprawdzamy przez ilu elfow punkt jest wspoldzielony
+                shared++;
+
+    return shared;
+}
+
+void f3b() {
+    FILE *file = fopen("data3", "r"); //deklaracja oraz otwarcie pliku z danymi
+    int l = 0, t = 0, w = 0, h = 0, n = 1;
+    char currentline[50];
+
+    while (fgets(currentline, sizeof(currentline), file) != NULL) {
+        sscanf(currentline, "%d%d%d%d", &l, &t, &w, &h);//'wyciaganie' danych z linii
+        if (sharedBy(l, t, w, h) == 0) { //jezeli zaden z punktow nie jest wspoldzielony z innym elfem to konczymy petle i wypisujemy wyniki
+            printf(" b. %d\n", n);
+            return;
+        }
+        n++;
+    }
+    fclose(file); //zamkniecie pliku
+}
+
+void f3() {
+    f3a();
+    f3b();
 }
