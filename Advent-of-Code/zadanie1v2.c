d9079bb73c77a9359d33c2881a01958e9fe2b3ec
Fr3sh98 <44109742+Fr3sh98@users.noreply.github.com>
2019-01-26 09:10:14 +0100

    Update zadanie1v2.c

--- Advent-of-Code/zadanie1v2.c
+++ Advent-of-Code/zadanie1v2.c
@@ -11,49 +11,48 @@ int n1 = 0; //wielkosc tablicy do zadania 1
 void f1();
 
 int main() {
-    f1();
-    return 0;
+  f1();
+  return 0;
 }
 
 void resetTable() {
-    for (int i = 0; i < 200000; i++)
-        numberFound[i] = 0;
+  for (int i = 0; i < 200000; i++)
+     numberFound[i] = 0;
 }
 
 void f1a() {
-    FILE *file = fopen("data1", "r");//deklaracja oraz otwarcie pliku z danymi
-    int a = 0;//zmienna przechowujaca suma liczb wejścoiwych
-    char currentline[1000];//buffor ktory jest w stanie przyjąć do 1000 znakków
+  FILE *file = fopen("data1", "r");//deklaracja oraz otwarcie pliku z danymi
+  int a = 0;//zmienna przechowujaca suma liczb wejścoiwych
+  char currentline[1000];//buffor ktory jest w stanie przyjąć do 1000 znakków
 
-    while (fgets(currentline, sizeof(currentline), file) !=NULL) { //pętla przechodząca przez plik i wczytujaca wszystkie linie az do konca pliku
-        int n = 0;
-        sscanf(currentline, "%d", &n);//wczytanie lini z pliku do zmiennej n
-        a += n; //dodawanie zmiennej n do zmiennej a
-        lines[n1] = n;
-        n1++; //zwiekszamy wielkosc tablicy
-    }
-    fclose(file); //zamkniecie pliku
-    printf("1) a. %d", a);
+  while (fgets(currentline, sizeof(currentline), file) !=NULL) { //pętla przechodząca przez plik i wczytujaca wszystkie linie az do konca pliku
+     int n = 0;
+     sscanf(currentline, "%d", &n);//wczytanie lini z pliku do zmiennej n
+     a += n; //dodawanie zmiennej n do zmiennej a
+     lines[n1] = n;
+     n1++; //zwiekszamy wielkosc tablicy
+  }
+  fclose(file); //zamkniecie pliku
+  printf("1) a. %d", a);
 }
 
 void f1b() {
-    int b = 0;//zmienna przechowujaca suma liczb wejścoiwych dla 2 całości z zadania
-    int found = 0;
-    int i = 0;
-    resetTable();
-    while (!found) { //petla dzialajaca do momentu znalezienia wartosci powtarzajacej sie 2 razy
-        b += lines[i];
-        numberFound[b + 100000]++;
-        if (numberFound[b + 100000] ==
-            2) { //jezeli liczba b wystapila 2 razy tp wypisujemy ja i wychodzimy z petli przez zmiana zmiennej found na 1
-            printf(" b. %d\n", b);
-            found = 1;
+  int b = 0;//zmienna przechowujaca suma liczb wejścoiwych dla 2 całości z zadania
+  int found = 0;
+  int i = 0;
+  resetTable();
+  while (!found) { //petla dzialajaca do momentu znalezienia wartosci powtarzajacej sie 2 razy
+     b += lines[i];
+     numberFound[b + 100000]++;
+     if (numberFound[b + 100000] == 2) { //jezeli liczba b wystapila 2 razy tp wypisujemy ja i wychodzimy z petli przez zmiana zmiennej found na 1
+       printf(" b. %d\n", b);
+       found = 1;
         }
-        i = (i + 1) % n1;
+     i = (i + 1) % n1;
     }
 }
 
 void f1() {
-    f1a();
-    f1b();
+  f1a();
+  f1b();
 }
