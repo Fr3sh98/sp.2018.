05859a0aaae0bf908e6fc6bfc214f959887c9583
Fr3sh98 <44109742+Fr3sh98@users.noreply.github.com>
2019-01-26 09:51:12 +0100

    Update zad7.c

--- budowanie-programow/zad7.c
+++ budowanie-programow/zad7.c
@@ -1,37 +1,44 @@
-/*Napisz program, który wczyta liczbę rzeczywistą i wypisze
- jej odwrotność oraz jej pierwiastek kwadratowy. Program nie
- powinien obliczać odwrotności liczby 0 oraz pierwiastka z
- liczby ujemnej. W takiej sytuacji zamiast obliczeń program
- powinien wypisać odpowiedni komunikat.*/
+// 7. Napisz program, który wczyta liczbę rzeczywistą
+// i wypisze jej odwrotność oraz jej pierwiastek kwadratowy.
+// Program nie powinien obliczać odwrotności liczby 0 oraz
+// pierwiastka z liczby ujemnej. W takiej sytuacji zamiast
+// obliczeń program powinien wypisać odpowiedni komunikat.
 
-#include<stdio.h>
-#include<math.h>
-#include"helper.h"
+#include <stdio.h>
+#include <math.h>
+
+void numberInput (double* number);
+void numberReverseResult (double number);
+void numberSqrtResult (double number);
 
 int main()
 {
-    double liczba, odwrotnosc, pierwiastek;
-    
-    wczytaj_double("podaj liczbe: " ,&liczba);
-    
-    odwrotnosc = 1/ liczba;
-    pierwiastek =sqrt(liczba);
-    
-    if(liczba == 0){
-        printf("dla liczby 0 nie ma odwrotnosci\n");
-        printf("liczba:%.2lf, pierwiastek: %.2lf\n",
-               liczba,  pierwiastek);
-    }
-    else if (liczba < 0){
-        printf("nie isnieje pierwiastek z liczby ujemniej %lf\n", liczba);
-        printf("liczba:%.2lf,odwrotnosc: %.2lf,\n",
-               liczba, odwrotnosc );
-        
-    }
-    else
-    {
-        printf("liczba:%.2lf,odwrotnosc: %.2lf, pierwiastek: %.2lf\n",
-               liczba, odwrotnosc, pierwiastek);
-    }
-    return 0;
+  double number;
+  printf ("Program wypisuje odwrotność oraz pierwiastek kwadratowy liczby podanej w konsoli \n ");
+  numberInput (&number);
+  if(number<0){
+    printf("Nie ma pierwiastka kwadratowego z liczby ujemnej.. \n");
+    numberReverseResult(number);
+  }else if(number==0){
+    printf("0 nie ma liczby odwrotnej do 0.. \n");
+    numberSqrtResult (number);
+  }else{
+    numberReverseResult(number);
+    numberSqrtResult(number);
+  }
+}
+
+void numberInput (double* number){
+  printf("Podaj liczbę rzeczywistą: \n");
+  scanf("%lf", number);
+}
+
+void numberReverseResult (double number){
+  double revereResult = 1/number;
+  printf("Odwrotnością liczby %.2lf jest liczba: %lf \n", number, revereResult);
+}
+
+void numberSqrtResult (double number){
+  double sqrtResult = sqrt(number);
+  printf("Pierwiastkiem liczby %.2lf jest liczba: %lf \n", number, sqrtResult);
 }
